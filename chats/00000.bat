c7c815745c0924db79e446f3f067c07c04e6a085
WinHack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2022-10-22 20:05:28 +0300

    Update 00000.bat

--- chats/00000.bat
+++ chats/00000.bat
@@ -3,12 +3,5 @@ title SysDataSoft Chat - SysDataMessenger
 cls
 echo SysDataSoft Chat===========================%time%=%date%
 echo (SDSBot)Welcome to chat!Its created!
-echo To update chat click any key...
+echo To exit click any key...
 pause >> nul
-del 00000.bat
-cd..
-wget --no-check-certificate "https://raw.githubusercontent.com/WinHack9702GitHub/SysDataMessengerServer/main/chats/00000.bat"
-copy 00000.bat Chats\00000.bat
-del 00000.bat
-cd chats
-00000.bat
