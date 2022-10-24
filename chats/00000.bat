6d0ad06a8e6dc41891db348bcf4721a8547bddae
WinHack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2022-10-24 16:57:11 +0300

    Delete 00000.bat

--- chats/00000.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-color DB
-title SysDataSoft Chat - SysDataMessenger
-cls
-echo SysDataSoft Chat===========================%time%=%date%
-echo (SDSBot)Welcome to chat!Its created!
-echo To exit click any key...
-pause >> nul
