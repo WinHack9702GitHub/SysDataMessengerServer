6ffd41488626991a824763ea5c6ac97f389da4e6
WinHack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2022-10-24 17:36:35 +0300

    Add files via upload

---
+++ chats/00000.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+color DB
+title SysDataSoft Chat - SysDataMessenger
+cls
+echo SysDataSoft Chat===========================%time%=%date%
+echo (SDSBot)Welcome to chat!Its created!
+echo (Guest2890424319)Egorka,privetik!(rus:Егорка, приветик!)
+echo (Egor)Привет!
+echo (Egor)Как делишки?
+echo (Egor)Для работы чата скоро будет необходима версия клиента 1.2 и выше!
+echo (Egor)офиц.сайт:sites.google.com/view/sysdatamessenger/главная-страница
+echo To exit click any key...
+pause >> nul
