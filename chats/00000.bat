78a30c64889522b2a4ecaf2549919aa728cd3263
WinHack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2022-10-24 17:09:00 +0300

    Add files via upload

---
+++ chats/00000.bat
@@ -0,0 +1,10 @@
+@echo off
+color DB
+title SysDataSoft Chat - SysDataMessenger
+cls
+echo SysDataSoft Chat===========================%time%=%date%
+echo (SDSBot)Welcome to chat!Its created!
+echo (Guest2890424319)Egorka,privetik!(rus:Егорка, приветик!)
+echo (Egor)Привет!
+echo To exit click any key...
+pause >> nul
